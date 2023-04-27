e0dd1cc18cef71696ff47a11a520b82427533270
pedro.henrique <pedro.henrique@brunsker.com.br>
2023-04-27 10:53:17 -0300

    Feat: Cdk table with forms

--- src/app/Examples/cdk-Table-Example/cdk-Table-Example.component.ts
+++ src/app/Examples/cdk-Table-Example/cdk-Table-Example.component.ts
@@ -10,6 +10,7 @@ import { PageEvent } from '@angular/material/paginator';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { COLORS, NAMES } from 'src/app/core/models';
 import { TableInlineComponent } from './table-inline/table-inline.component';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 export interface UserData {
   id: string;
@@ -38,52 +39,75 @@ export class CdkTableExampleComponent implements OnInit {
     this.tablelength = this.exampleDatabase.data.length;
   }
 
+  logrow(row: any){
+    this.dataSource!.connect().subscribe(console.log)
+    console.log(row, );
+    
+  }
+
   ngOnInit() {
-    this.handlePageEvent({ pageIndex: 0, pageSize: 10, length: 100 });
+    //this.handlePageEvent({ pageIndex: 0, pageSize: 10, length: 100 });
   }
 
-  handlePageEvent(e: PageEvent) {
-    this.containers?.toArray().forEach((element) => element.clear());
-    this.expandedRow = [];
-    const CopiedData = this.exampleDatabase.DefaultData;
-    const index = e.pageIndex * e.pageSize;
-    this.exampleDatabase.dataChange.next(
-      CopiedData.slice(index, index + e.pageSize)
-    );
-  }
+  // handlePageEvent(e: PageEvent) {
+  //   this.containers?.toArray().forEach((element) => element.clear());
+  //   this.expandedRow = [];
+  //   const CopiedData = this.exampleDatabase.DefaultData;
+  //   const index = e.pageIndex * e.pageSize;
+  //   this.exampleDatabase.dataChange.next(
+  //     CopiedData.slice(index, index + e.pageSize)
+  //   );
+  // }
 
-  expandRow(index: number) {
-    if (this.expandedRow.indexOf(index) > -1) {
-      this.containers.toArray()[index]?.clear();
-      this.expandedRow = this.expandedRow.filter((x) => x != index);
-      return;
-    }
-    this.expandedRow.push(index);
-    const messageComponent = this.containers
-      .toArray()
-      [index]?.createComponent(TableInlineComponent);
-    messageComponent.instance.user = this.exampleDatabase.data[index].name;
-  }
+  // expandRow(index: number) {
+  //   if (this.expandedRow.indexOf(index) > -1) {
+  //     this.containers.toArray()[index]?.clear();
+  //     this.expandedRow = this.expandedRow.filter((x) => x != index);
+  //     return;
+  //   }
+  //   this.expandedRow.push(index);
+  //   const messageComponent = this.containers
+  //     .toArray()
+  //     [index]?.createComponent(TableInlineComponent);
+  //   messageComponent.instance.user = this.exampleDatabase.data[index].name;
+  // }
 }
 
 export class ExampleDatabase {
-  dataChange: BehaviorSubject<UserData[]> = new BehaviorSubject<UserData[]>([]);
+  TableForm = new FormGroup({
+    TableFromArray: new FormArray([]),
+  });
+
+  get TableFromArray() {
+    return this.TableForm.get('TableFromArray') as FormArray;
+  }
+  dataChange: BehaviorSubject<any> = new BehaviorSubject<any>(null as any);
   DefaultData: UserData[] = [];
-  get data(): UserData[] {
-    return this.dataChange.value;
+
+  get data(): FormArray<any> {
+    return this.dataChange.value?.value ?? [];
   }
 
   constructor() {
     for (let i = 0; i < 100; i++) {
       this.addUser();
     }
+    console.log("Form", this.TableFromArray.controls);
+    
   }
 
   addUser() {
-    const copiedData = this.data.slice();
-    copiedData.push(this.createNewUser());
-    this.DefaultData = copiedData;
-    this.dataChange.next(copiedData);
+
+    const newUser = this.createNewUser();
+    const newForm = new FormGroup({
+      id: new FormControl(newUser.id),
+      name: new FormControl(newUser.name),
+      progress: new FormControl(newUser.progress),
+      color: new FormControl(newUser.color),
+    });
+    this.TableFromArray.push(newForm);
+
+    this.dataChange.next(this.TableFromArray.controls);
   }
 
   public createNewUser() {
@@ -109,11 +133,11 @@ export class ExampleDatabase {
  * the underlying data. Instead, it only needs to take the data and send the table exactly what
  * should be rendered.
  */
-export class ExampleDataSource extends DataSource<any> {
+export class ExampleDataSource extends DataSource<FormArray<any>> {
   constructor(private _exampleDatabase: ExampleDatabase) {
     super();
   }
-  connect(): Observable<UserData[]> {
+  connect(): Observable<any> {
     return this._exampleDatabase.dataChange;
   }
   disconnect() {}
